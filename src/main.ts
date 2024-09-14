7cd60b592f8542b41214c50c2a1fc366c1f83941
inogai <inogai.sign@gmail.com>
2024-09-14 12:34:04 +0800

    add v-wave

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import '@/assets/index.css'
 
+import VWave from 'v-wave'
 import { createApp } from 'vue'
 
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(VWave, {})
+  .mount('#app')
