98acc6e154123cc5aaedb05edfca353cfc7dc806
inogai <inogai.sign@gmail.com>
2024-09-14 11:12:01 +0800

    configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,43 @@
 import antfu from '@antfu/eslint-config'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
-export default antfu({
-  formatters: true,
-  vue: true,
-})
+export default antfu(
+  {
+    type: 'app',
+    stylistic: {
+      overrides: {
+        'style/max-len': ['error', { code: 80 }],
+        'style/padded-blocks': ['error', 'never'],
+        'style/padding-line-between-statements': 'error',
+        'style/lines-between-class-members': ['error', {
+          enforce: [
+            { blankLine: 'always', prev: '*', next: '*' },
+            { blankLine: 'never', prev: 'field', next: 'field' },
+          ],
+        }],
+      },
+    },
+    test: {
+      overrides: {
+        'test/prefer-lowercase-title': ['error', {
+          ignoreTopLevelDescribe: true,
+        }],
+      },
+    },
+    typescript: {
+      overrides: {
+        'ts/no-redeclare': 'off', // handled by typescript
+      },
+    },
+    formatters: true,
+  },
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+)
