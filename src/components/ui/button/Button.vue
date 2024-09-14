dac3f99e44646370f2ed01226fe9149d99be4c61
inogai <inogai.sign@gmail.com>
2024-09-14 12:48:18 +0800

    feat: add v-wave to button

--- src/components/ui/button/Button.vue
+++ src/components/ui/button/Button.vue
@@ -19,6 +19,7 @@ const props = withDefaults(defineProps<Props>(), {
 
 <template>
   <Primitive
+    v-wave
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
