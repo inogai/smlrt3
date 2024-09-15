bbeb4f01b5f87aabc629e95c9c09a22a62404e4b
inogai <inogai.sign@gmail.com>
2024-09-15 23:49:10 +0800

    feat: render Kmb Eta

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+import EtaItem from '@/components/EtaItem.vue'
+
 import type { TEta } from '@/apis/base'
 import { getCurrentPosition } from '@/apis/geolocation'
 import { KmbApi } from '@/apis/kmb'
-import { Button } from '@/components/ui/button'
 import { Ok } from '@/lib/results'
 
 const kmb = new KmbApi()
 
-const etaEntries: TEta[] = []
+const etaEntries = ref<TEta[]>([])
 
 async function fetchData() {
   const pos = await getCurrentPosition()
   const res = await kmb.getNearbyEtas(pos.coords.latitude, pos.coords.longitude)
 
   res.andThen((data) => {
-    etaEntries.push(...data)
+    etaEntries.value.push(...data)
     return Ok()
   })
 }
@@ -22,7 +25,12 @@ fetchData()
 </script>
 
 <template>
-  <Button variant="destructive">
-    Click Me
-  </Button>
+  <div>
+    <template
+      v-for="(item, index) in etaEntries"
+      :key="index"
+    >
+      <EtaItem :item="item" />
+    </template>
+  </div>
 </template>
---
+++ src/components/EtaItem.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { isKind } from 'breath-enum'
+
+import type { TEta } from '@/apis/base'
+import { EtaDescriptor } from '@/apis/EtaDescriptor'
+
+const { item } = defineProps<{
+  item: TEta
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col px-4">
+    <div
+      class="border-t"
+    />
+    <div class="flex items-baseline gap-4 p-2">
+      <div class="flex grow items-center justify-start">
+        <div
+          class="
+            flex w-12 justify-center rounded-full bg-red-600 text-sm
+            font-semibold text-white
+          "
+        >
+          {{ item.route().name() }}
+        </div>
+        <div class="w-4" />
+        <div class="flex flex-col text-sm">
+          <div>
+            往&nbsp;
+            <span class="text-lg font-semibold text-primary">
+              {{ item.route().dest() }}
+            </span>
+          </div>
+          <div>{{ item.station().name() }}</div>
+        </div>
+      </div>
+      <div class="flex items-end justify-end text-muted-foreground">
+        <div>
+          <span
+            v-for="(eta, ind) in item.items()"
+            :key="ind"
+            :class="{
+              'text-xl font-bold text-primary':
+                (ind === 0 || item.items().slice(0, ind).every(
+                  ({ val }) => {
+                    return isKind(val, EtaDescriptor.JustDeparted)
+                  },
+                )) && isKind(eta.val, EtaDescriptor.MinutesLeft),
+            }"
+          >
+            <span
+              v-if="isKind(eta.val, EtaDescriptor.JustDeparted)" class="text-sm"
+            >
+              正在離開</span>
+            <template v-else-if="isKind(eta.val, EtaDescriptor.MinutesLeft)">
+              {{ eta.val.unwrapAs(EtaDescriptor.MinutesLeft) }}
+            </template>
+            <template v-else-if="isKind(eta.val, EtaDescriptor.NoEta)">
+              沒有班次
+            </template>
+            <template v-if="ind !== item.items().length - 1">,&nbsp;</template>
+          </span>
+          <span
+            v-if="isKind (item.items()[item.items().length - 1].val,
+                          EtaDescriptor.MinutesLeft)"
+          >
+            分鐘
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
