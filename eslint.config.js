4ea161d7aef770083bf9eed60919ebdd7e12f996
inogai <inogai.sign@gmail.com>
2024-09-15 23:48:57 +0800

    chore: eslint import sort rule

--- eslint.config.js
+++ eslint.config.js
@@ -37,7 +37,26 @@ export default antfu(
       'simple-import-sort': simpleImportSort,
     },
     rules: {
-      'simple-import-sort/imports': 'error',
+      'simple-import-sort/imports': ['error', {
+        groups: [
+          // Side effect imports.
+          ['^\\u0000'],
+          // Node.js builtins prefixed with `node:`.
+          ['^node:'],
+          // Packages.
+          // Things that start with a letter (or digit or underscore),
+          // or `@` followed by a letter.
+          ['^@?\\w'],
+          // Vue Components Import
+          ['\\.vue$'],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything not matched in another group.
+          ['^'],
+          // Relative imports.
+          // Anything that starts with a dot.
+          ['^\\.'],
+        ],
+      }],
       'simple-import-sort/exports': 'error',
       'perfectionist/sort-imports': 'off',
     },
