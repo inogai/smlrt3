bd7979e8d3cc394c1dd366ed412f912f4320d898
inogai <inogai.sign@gmail.com>
2024-09-18 00:22:08 +0800

    ref(SettingsView): use SettingsForm

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,38 +1,24 @@
 <script setup lang="ts">
-import { toTypedSchema } from '@vee-validate/zod'
-import { useForm } from 'vee-validate'
+import { ref } from 'vue'
 
-import { AutoForm } from '@/components/ui/auto-form'
+import SettingsForm from '@/components/SettingsForm.vue'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
 
-import type { ISettings } from '@/settings'
-import { settingsSchema, useSettings } from '@/settings'
+const triggerSubmit = ref(false)
 
-const settings = useSettings()
-
-const form = useForm({
-  validationSchema: toTypedSchema(settingsSchema),
-})
-
-function onSubmit(values: Record<string, unknown>) {
-  settings.value = values as ISettings
+function submit() {
+  triggerSubmit.value = true
 }
 </script>
 
 <template>
   <Card>
     <CardContent>
-      <AutoForm
-        :schema="settingsSchema"
-        :form="form"
-        class="space-y-6"
-        @submit="onSubmit"
-      >
-        <Button type="submit">
-          Apply
-        </Button>
-      </AutoForm>
+      <SettingsForm v-model:trigger-submit="triggerSubmit" />
     </CardContent>
+    <Button @click="submit()">
+      Apply
+    </Button>
   </Card>
 </template>
