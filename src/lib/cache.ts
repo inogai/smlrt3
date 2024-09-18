65cac95714477ccc5c925217e6b920733dd28070
inogai <inogai.sign@gmail.com>
2024-09-18 13:53:52 +0800

    ref(cache): replace localStorage with idb-keyval

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,3 +1,5 @@
+import * as idb from 'idb-keyval'
+
 interface RefreshedObject<T> {
   value: T
   useCached: false
@@ -16,7 +18,7 @@ async function refreshCache<T>(
 ): Promise<RefreshedObject<T>> {
   const ret = await callback()
   const timestamp = Date.now()
-  localStorage.setItem(key, JSON.stringify(ret))
+  idb.set(key, ret)
   localStorage.setItem(`${key}_timestamp`, timestamp.toString())
   return { value: ret, useCached: false, lastUpdate: timestamp }
 }
@@ -32,12 +34,12 @@ export async function cache<T>(
   if (Date.now() - timestamp > ttl * 1000)
     return refreshCache(key, callback)
 
-  const str = localStorage.getItem(key)
-  if (!str)
-    return refreshCache(key, callback)
-
   try {
-    const data = JSON.parse(str)
+    const data = await idb.get(key)
+
+    if (data === undefined)
+      return refreshCache(key, callback)
+
     return {
       value: data,
       useCached: true,
