8d9c74753b4f837b925c6d0b40823d25364468f6
inogai <inogai.sign@gmail.com>
2024-09-20 16:53:05 +0800

    ref: move traits

--- src/components/ui/tags-input/TagsInputItem.vue
+++ src/components/ui/tags-input/TagsInputItem.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-import { type HTMLAttributes, computed } from 'vue'
-import { TagsInputItem, type TagsInputItemProps, useForwardProps } from 'radix-vue'
+import {
+  TagsInputItem,
+  type TagsInputItemProps,
+  useForwardProps,
+} from 'radix-vue'
+import { computed, type HTMLAttributes } from 'vue'
 
 import { cn } from '@/lib/utils'
 
-const props = defineProps<TagsInputItemProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<
+  & TagsInputItemProps
+  & { class?: HTMLAttributes['class'] }
+  & { variant?: 'default' | 'outlined' }
+>()
 
 const delegatedProps = computed(() => {
   const { class: _, ...delegated } = props
@@ -16,7 +24,13 @@ const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
 <template>
-  <TagsInputItem v-bind="forwardedProps" :class="cn('flex h-6 items-center rounded bg-secondary data-[state=active]:ring-ring data-[state=active]:ring-2 data-[state=active]:ring-offset-2 ring-offset-background', props.class)">
+  <TagsInputItem
+    v-bind="forwardedProps" :class="cn(`
+      flex h-6 items-center rounded bg-secondary ring-offset-background
+      data-[state=active]:ring-2 data-[state=active]:ring-ring
+      data-[state=active]:ring-offset-2
+    `, props.class)"
+  >
     <slot />
   </TagsInputItem>
 </template>
